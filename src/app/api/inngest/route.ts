8daa31b459b14070db61e7cb4c6ba9245a9144c8
t3ngho00 <t3ngho00@students.oamk.fi>
2026-08-17 13:05:00 +0300

    conversation system

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,7 @@ import { serve } from "inngest/next";
 
 import { inngest } from "@/inngest/client";
 import { demoError, demoGenerate } from "@/inngest/functions";
+import { processMessage } from "@/features/conversations/inngest/process-message";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -9,5 +10,6 @@ export const { GET, POST, PUT } = serve({
   functions: [
     demoGenerate,
     demoError,
+    processMessage,
   ],
 });
